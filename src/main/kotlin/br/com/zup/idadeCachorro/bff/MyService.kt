64c2478339f61cea88c27a21838cffd5dad1fb02
suricatoti <alexandredslima@gmail.com>
2020-08-17 16:07:25 -0300

    Layout correction

--- src/main/kotlin/br/com/zup/idadeCachorro/bff/MyService.kt
+++ src/main/kotlin/br/com/zup/idadeCachorro/bff/MyService.kt
@@ -22,13 +22,15 @@ import br.com.zup.beagle.widget.context.valueOf
 import br.com.zup.beagle.widget.core.*
 import br.com.zup.beagle.widget.layout.Container
 import br.com.zup.beagle.widget.layout.Screen
-import br.com.zup.beagle.widget.layout.ScreenBuilder
 import br.com.zup.beagle.widget.ui.*
 import org.springframework.stereotype.Service
 
 @Service
 class MyService {
 
+    /**
+     * Armazena o valor digitado na tela nessa variável
+     */
     data class Variables(
             val dogAge: String
     )
@@ -39,42 +41,48 @@ class MyService {
     fun createWidget(): Widget = Container(
             context = ContextData(id = "variables", value = Variables(dogAge = "")),
             children = listOf(
-            Image(
-                    path =
-                    ImagePath.Local.justMobile(
-                            mobileId = "cachorro"
-                    )
-            ).applyStyle(Style(size = Size(
-                    width = 160.unitReal(),
-                    height = 160.unitReal()
-            ))).applyFlex(Flex(alignSelf = AlignSelf.CENTER)),
-            Container(listOf(
-                    Text(text = "Digite a idade do seu cachorro, para descobrir a idade em anos humanos.")
-            ))
-                    .applyStyle(style = Style(backgroundColor = "#52c8e3", size = Size(80.unitPercent()),
-                            cornerRadius = CornerRadius(20.0),
-                            padding = EdgeValue(
-                                    all = 20.unitReal()
-                            )
-                    ))
-                    .applyFlex(Flex(alignContent = AlignContent.CENTER)),
-            TextInput(placeholder = "Digite a idade do cachorro", onChange = listOf(
-                    //Armazenar o valor no contexto global do beagle
-                    SetContext(contextId = "variables", path = "dogAge", value = "@{onChange.value}")
-            ))
-                    .applyStyle(style = Style(backgroundColor = "#FFFFFF", size = Size(height = 120.unitReal(), width = 50.unitPercent()),
-                            cornerRadius = CornerRadius(20.0),
-                            margin = EdgeValue(
-                                    horizontal = 20.unitReal(),
-                                    vertical = 50.unitReal()
-                            )
-                    )),
-                    Text(text = "@{variables.dogAge}"),
-            Button(text = "Descobrir a idade", onPress = listOf(
-                    SendRequest(url = "/calculadora/@{variables.dogAge}", method = RequestActionMethod.GET,
-                            onSuccess = listOf(Alert(title = "", message = "A idade do cachorro é @{onSuccess.data} anos")),
-                    onError = listOf(Alert(title = "ERRO", message = "Erro")))
-    ))
-            .applyStyle(Style(flex = Flex(grow = 1.0)))
-            .applyFlex(Flex(alignContent = AlignContent.CENTER, alignItems = AlignItems.CENTER))))
+                Image(
+                        path =
+                        ImagePath.Local.justMobile(
+                                mobileId = "cachorro"
+                        )
+                ).applyStyle(Style(size = Size(
+                        width = 160.unitReal(),
+                        height = 160.unitReal()
+                ))).applyFlex(Flex(alignSelf = AlignSelf.CENTER)),
+
+                Container(listOf(
+                        Text(text = "Digite a idade do seu cachorro, para descobrir a idade em anos humanos.")
+                ))
+                .applyStyle(style = Style(backgroundColor = "#52c8e3", size = Size(80.unitPercent()),
+                        cornerRadius = CornerRadius(20.0),
+                        padding = EdgeValue(
+                                all = 20.unitReal()
+                        )
+                ))
+                        .applyFlex(Flex(alignSelf = AlignSelf.CENTER)),
+
+                TextInput(placeholder = "Digite a idade do cachorro", onChange = listOf(
+                        //Armazenar o valor no contexto global do beagle
+                        SetContext(contextId = "variables", path = "dogAge", value = "@{onChange.value}")
+                ))
+                        .applyFlex(Flex(alignSelf = AlignSelf.CENTER))
+                        .applyStyle(style = Style(backgroundColor = "#FFFFFF",
+                                size = Size(height = 60.unitReal(), width = 60.unitPercent()),
+                                cornerRadius = CornerRadius(20.0),
+                                margin = EdgeValue(
+                                        vertical = 50.unitReal()
+                                )
+                )),
+
+                Button(text = "Descobrir a idade", onPress = listOf(
+                        SendRequest(url = "/calculadora/@{variables.dogAge}", method = RequestActionMethod.GET,
+                                onSuccess = listOf(
+                                        Alert(title = "", message = "A idade do cachorro em anos humanos é: @{onSuccess.data} anos")),
+                                onError = listOf(
+                                        Alert(title = "ERRO", message = "Erro")))
+                ))
+                    .applyStyle(Style(flex = Flex(grow = 1.0)))
+                    .applyFlex(Flex(alignContent = AlignContent.CENTER, alignItems = AlignItems.CENTER)))
+    )
 }
